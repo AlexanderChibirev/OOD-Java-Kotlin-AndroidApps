88f4c258d5d2508b8f7931c40d1417af96393c0e
unknown <www.darmagh@mail.ru>
2016-08-15 02:20:01 +0300

    добавлены все нужные активити

--- Decorator/app/src/main/java/com/example/alexander/decorator/LiquorActivity.kt
+++ Decorator/app/src/main/java/com/example/alexander/decorator/LiquorActivity.kt
@@ -1,13 +1,23 @@
 package com.example.alexander.decorator
-
+import android.content.Intent
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.ListView
-import com.example.alexander.decorator.Definitions.LiquorType
+import android.widget.RadioGroup
+import kotlinx.android.synthetic.main.activity_liquor.*
 
 class LiquorActivity : AppCompatActivity() {
-    var typess = LiquorType.values()
-    //val condimentNames = arrayOf()
-    val lvCondiment = findViewById(R.id.listViewCondiment) as ListView
-    //val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, condimentNames)
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_liquor)
+        val radioGroup = findViewById(R.id.radioGroupLiquor) as RadioGroup
+        radioGroup.setOnCheckedChangeListener(object : RadioGroup.OnCheckedChangeListener {
+            override fun onCheckedChanged(group: RadioGroup, checkedId: Int) {
+                // TODO Auto-generated method stub
+            }
+        })
+        buttonAdderLiquor.setOnClickListener {
+            val intent = Intent(this, CondimentsActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
--- Decorator/app/src/main/java/com/example/alexander/decorator/SyrupActivity.kt
+++ Decorator/app/src/main/java/com/example/alexander/decorator/SyrupActivity.kt
@@ -1,12 +1,25 @@
 package com.example.alexander.decorator
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.RadioGroup
+import kotlinx.android.synthetic.main.activity_syrup.*
 
 class SyrupActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_syrup)
+        val radioGroup = findViewById(R.id.radioGroupSyrup) as RadioGroup
+        radioGroup.setOnCheckedChangeListener(object : RadioGroup.OnCheckedChangeListener {
+            override fun onCheckedChanged(group: RadioGroup, checkedId: Int) {
+                // TODO Auto-generated method stub
+            }
+        })
+        buttonAdderSyrup.setOnClickListener {
+            val intent = Intent(this, CondimentsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
