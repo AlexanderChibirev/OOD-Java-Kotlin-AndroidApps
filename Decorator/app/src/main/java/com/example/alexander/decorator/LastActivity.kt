ac9a0c0824f832e6b4ab37572c840e3afa7b458b
unknown <www.darmagh@mail.ru>
2016-08-16 12:15:40 +0300

    добавил скрол бар для большого заказа

--- Decorator/app/src/main/java/com/example/alexander/decorator/LastActivity.kt
+++ Decorator/app/src/main/java/com/example/alexander/decorator/LastActivity.kt
@@ -3,6 +3,7 @@ package com.example.alexander.decorator
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.method.ScrollingMovementMethod
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_last.*
 
@@ -12,10 +13,10 @@ class LastActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_last)
         var materialOrder = findViewById(R.id.textViewOrder) as TextView
+        materialOrder.setMovementMethod(ScrollingMovementMethod())
         materialOrder.setText(intent.getStringExtra("materialOrder"))
         var costOrder = findViewById(R.id.textViewCostOrder) as TextView
         costOrder.setText("Итог: " + intent.getStringExtra("costOrder") + " рублей")
-
         buttonBeginActivity.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
             this@LastActivity.finish();
