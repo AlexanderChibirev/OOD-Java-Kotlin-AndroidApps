63d53adf01b385db5aaf9995010bb647892c2632
Alexander <www.darmagh@mail.ru>
2017-01-19 23:32:11 +0300

    add IPainter and IShapeFactory interface

---
+++ ShapesPainter/app/src/main/java/com/example/alexander/shapespainter/IPainter.java
@@ -0,0 +1,9 @@
+package com.example.alexander.shapespainter;
+
+/**
+ * Created by Alexander on 19.01.2017.
+ */
+
+public interface IPainter {
+    void drawPicture(PictureDraft draft, ICanvas canvas);
+}
---
+++ ShapesPainter/app/src/main/java/com/example/alexander/shapespainter/IShapeFactory.java
@@ -0,0 +1,15 @@
+package com.example.alexander.shapespainter;
+
+import android.graphics.Color;
+
+/**
+ * Created by Alexander on 19.01.2017.
+ */
+
+interface IShapeFactory {
+    void createShape(final Vector2f center,
+                     final Vector2f size,
+                     final Color fillColor,
+                     final Color outlineColor,
+                     final ShapeType shapeType);
+}
