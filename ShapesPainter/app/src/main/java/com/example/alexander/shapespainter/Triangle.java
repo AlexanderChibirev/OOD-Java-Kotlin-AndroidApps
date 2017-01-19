5660e9400a52c6ff20ef266f1fb65ae59aa59694
Alexander <www.darmagh@mail.ru>
2017-01-20 00:13:01 +0300

    add class Triangle

--- ShapesPainter/app/src/main/java/com/example/alexander/shapespainter/Triangle.java
+++ ShapesPainter/app/src/main/java/com/example/alexander/shapespainter/Triangle.java
@@ -1,8 +1,59 @@
 package com.example.alexander.shapespainter;
 
+import android.graphics.Color;
+
 /**
  * Created by Alexander on 20.01.2017.
  */
 
-public class Triangle {
+public class Triangle extends Shape  {
+
+    public Triangle(Vector2f center, VectorSize2f size, Color fillColor, Color outlineColor) {
+
+    }
+
+    @Override
+    void draw(ICanvas canvas) {
+
+    }
+
+    @Override
+    public ShapeType getType() {
+        return null;
+    }
+
+    @Override
+    public ShapeDiagram getDiagram() {
+        return null;
+    }
+
+    @Override
+    public Color getFillColor() {
+        return null;
+    }
+
+    @Override
+    public Color getOutlineColor() {
+        return null;
+    }
+
+    @Override
+    public Vector2f getVertices() {
+        return null;
+    }
+
+    @Override
+    public boolean isPointInside(Vector2f point) {
+        return false;
+    }
+
+    @Override
+    public Vector2f getCenter() {
+        return null;
+    }
+
+    @Override
+    public Vector2f getSize() {
+        return null;
+    }
 }
