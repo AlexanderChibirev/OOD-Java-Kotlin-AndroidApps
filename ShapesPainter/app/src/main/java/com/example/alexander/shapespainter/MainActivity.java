8f9e738d34a38b1e749afd46062bcf067ab64b0f
Alexander <www.darmagh@mail.ru>
2017-02-04 17:14:17 +0300

    1) удален gridview и его адаптор(неудобно использовать) заменено на простые imageButton 2) изменен размер изабражений toolbars 3) добавлен слушатель для кнопок, все кнопки toolbar-ов находятся в одном массиве и в зависимости от имени кнопки определяют свое действие 4) исправлены варненги missing contentdescription attribute on imag(добавил в html string alt tag-и)

--- ShapesPainter/app/src/main/java/com/example/alexander/shapespainter/MainActivity.java
+++ ShapesPainter/app/src/main/java/com/example/alexander/shapespainter/MainActivity.java
@@ -3,41 +3,69 @@ package com.example.alexander.shapespainter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.alexander.shapespainter.view.PainterCanvas;
 
+import java.util.Vector;
+
 public class MainActivity extends AppCompatActivity {
     PainterCanvas mySurfaceView;
+    Vector<ImageButton> mImageButtons = new Vector<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mySurfaceView  = (PainterCanvas)findViewById(R.id.myview);
-
-
-        GridView gridview = (GridView) findViewById(R.id.redo_undo_trash_tools);
-        gridview.setAdapter(new ImageToolsAdapter(this));
-
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                Toast.makeText(MainActivity.this, "" + position,
-                        Toast.LENGTH_SHORT).show();
-                switch(position) {
-                    case 0:
-                        break;
-                    case 1:
-                        break;
-                    case 2:
-                        break;
-                }
-            }
-        });
+        mySurfaceView = (PainterCanvas) findViewById(R.id.myView);
+        initButtonToolbars();
+        onClick();
     }
 
+    private void onClick() {
+        for (final ImageButton imageButton : mImageButtons) {
+            imageButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int imageButtonId = imageButton.getId();
+                    if (imageButtonId == R.id.imageButtonTriangle) {
+                        Toast.makeText(getApplicationContext(),
+                                "triangle",
+                                Toast.LENGTH_SHORT).show();
+                    } else if (imageButtonId == R.id.imageButtonCircle) {
+                        Toast.makeText(getApplicationContext(),
+                                "circle",
+                                Toast.LENGTH_SHORT).show();
+                    } else if (imageButtonId == R.id.imageButtonRectangle) {
+                        Toast.makeText(getApplicationContext(),
+                                "rectangle",
+                                Toast.LENGTH_SHORT).show();
+                    } else if (imageButtonId == R.id.imageButtonUndo) {
+                        Toast.makeText(getApplicationContext(),
+                                "undo",
+                                Toast.LENGTH_SHORT).show();
+                    } else if (imageButtonId == R.id.imageButtonRedo) {
+                        Toast.makeText(getApplicationContext(),
+                                "redo",
+                                Toast.LENGTH_SHORT).show();
+                    } else if (imageButtonId == R.id.imageButtonTrash) {
+                        Toast.makeText(getApplicationContext(),
+                                "trash",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
 
+    private void initButtonToolbars() {
+        mImageButtons.add((ImageButton) findViewById(R.id.imageButtonTriangle));
+        mImageButtons.add((ImageButton) findViewById(R.id.imageButtonCircle));
+        mImageButtons.add((ImageButton) findViewById(R.id.imageButtonRectangle));
+        mImageButtons.add((ImageButton) findViewById(R.id.imageButtonUndo));
+        mImageButtons.add((ImageButton) findViewById(R.id.imageButtonRedo));
+        mImageButtons.add((ImageButton) findViewById(R.id.imageButtonTrash));
+    }
 }
