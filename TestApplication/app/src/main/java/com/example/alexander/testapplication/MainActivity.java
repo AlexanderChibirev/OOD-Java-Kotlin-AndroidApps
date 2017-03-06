807658577f17c634f9702c163fc7ae85707e5cda
Alexander <www.darmagh@mail.ru>
2017-03-07 01:38:32 +0300

    add preference, class contextWrapper for change correct localization, ru-en localization html, empty activityes for sub view

--- TestApplication/app/src/main/java/com/example/alexander/testapplication/MainActivity.java
+++ TestApplication/app/src/main/java/com/example/alexander/testapplication/MainActivity.java
@@ -1,7 +1,17 @@
 package com.example.alexander.testapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_preference:
+                showPreferences();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences preferences = PreferenceManager
+                .getDefaultSharedPreferences(this);
+
+        String lang = preferences.getString(
+                getString(R.string.preferences_language_key), null);
+
+        if (lang != null) {
+            Locale locale = new Locale(lang);
+            Locale.setDefault(locale);
+            Configuration config = new Configuration();
+            config.locale = locale;
+            getBaseContext().getResources().updateConfiguration(config, null);
+        }
+        setTitle(R.string.app_name);
+    }
+
+    private void showPreferences() {
+        Intent intent = new Intent();
+        intent.setClass(this, Preferences.class);
+        startActivity(intent);
     }
 }
